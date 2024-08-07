296db00b351edb7cf1a45fefd874426eea191dac
Liao1492 <liaohuaxin1492@gmail.com>
2024-08-07 14:51:49 +0800

    end chapter 6

---
+++ src/main/kotlin/lox/AstPrinter.kt
@@ -0,0 +1,60 @@
+package lox
+
+import lox.expr.Expr
+
+class AstPrinter : Expr.Visitor<String>{
+
+    fun print(expr: Expr): String = expr.accept(this)
+
+    override fun visitExpr(expr: Expr.Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitExpr(expr: Expr.Grouping): String {
+        return parenthesize("group", expr.expression)
+    }
+
+    override fun visitExpr(expr: Expr.Literal): String {
+        return expr.value?.toString() ?: "nil"
+    }
+
+    override fun visitExpr(expr: Expr.Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right)
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        return buildString {
+            append("(")
+            append(name)
+            for (expr in exprs) {
+                append(" ")
+                // here need to qualify `this` with a tag b/c we're within a buildString
+                append(expr.accept(this@AstPrinter))
+            }
+            append(")")
+        }
+    }
+//    private fun parenthesize(name:String,  vararg exprs: Expr){
+//        return buildString {
+//            append("(")
+//            append(name)
+//            for (expr in exprs) {
+//                append(" ")
+//                // here need to qualify `this` with a tag b/c we're within a buildString
+//                append(expr.accept(this@AstPrinter))
+//            }
+//            append(")")
+//        }
+//    }
+}
+
+fun main() {
+    val expression = Expr.Binary(
+        Expr.Unary(
+            Token(TokenType.MINUS, "-", null, 1), Expr.Literal(123)
+        ), Token(TokenType.STAR, "*", null, 1), Expr.Grouping(
+            Expr.Literal(45.67)
+        )
+    )
+    println(AstPrinter().print(expression)) // (* (- 123) (group 45.67))
+}
